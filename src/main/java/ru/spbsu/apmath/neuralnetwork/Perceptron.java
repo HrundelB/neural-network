070076b24e7b19269bb61dfdd2cccea095fd57d1
Afonin Sergey <hrundelb@yandex.ru>
2014-10-10 00:38:04 +0400

    rft - Using commons intead of JAMA

--- src/main/java/ru/spbsu/apmath/neuralnetwork/Perceptron.java
+++ src/main/java/ru/spbsu/apmath/neuralnetwork/Perceptron.java
@@ -1,9 +1,15 @@
 package ru.spbsu.apmath.neuralnetwork;
 
-import Jama.Matrix;
+import com.spbsu.commons.math.vectors.Mx;
+import com.spbsu.commons.math.vectors.Vec;
+import com.spbsu.commons.math.vectors.impl.mx.RowsVecArrayMx;
+import com.spbsu.commons.math.vectors.impl.vectors.ArrayVec;
+import com.spbsu.commons.math.vectors.impl.vectors.VecBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.spbsu.commons.math.vectors.VecTools.multiply;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,82 +19,99 @@ import java.util.List;
  */
 public class Perceptron {
   private int countOfLayers;
-  private Lawyer[] layers;
+  private Layer[] layers;
   private List<Integer> hiddenLayersDims;
 
   public Perceptron(List<Integer> hiddenLayersDims) {
     this.hiddenLayersDims = hiddenLayersDims;
     this.countOfLayers = hiddenLayersDims.size() + 1;  //hidden layers + 1 output
-    this.layers = new Lawyer[countOfLayers];
+    this.layers = new Layer[countOfLayers];
   }
 
-  public Matrix calculate(Matrix input) {
+  public double calculate(Vec input) {
     if (layers[0] == null) {
       throw new RuntimeException("Perceptron must be learned before work!");
     }
     layers[0].calculate(input);
     for (int i = 1; i < countOfLayers; i++)
       layers[i].calculate(layers[i-1].getOutputs());
-    return layers[countOfLayers - 1].getOutputs();
+    return layers[countOfLayers - 1].getOutputs().get(0);
   }
 
-  public void backPropagation(double n, Matrix[] learning, Matrix[] answers, int numberOfSteps, Function p) {
-    int inputDim = learning[0].getRowDimension();
-    int outputDim = answers[0].getRowDimension();
+  public void backPropagation(double n, Vec[] learning, int[] answers, int numberOfSteps) {
+    int inputDim = learning[0].dim();
     int countOfLearningSamples = learning.length;
-    initWeights(inputDim, outputDim);
-    for (int i = 0; i < numberOfSteps; i++) {
+    initWeights(inputDim);
+    for (int k = 0; k < numberOfSteps; k++) {
       for (int d = 0; d < countOfLearningSamples; d++){
         this.calculate(learning[d]);
-        Matrix[] deltas = new Matrix[countOfLayers];
-        deltas[countOfLayers - 1] = getDeltaForLastLayer(answers[d], p);
+        Vec[] deltas = new Vec[countOfLayers];
+        deltas[countOfLayers - 1] = getDeltaForLastLayer(answers[d]);
         for (int l = countOfLayers - 2; l >= 0; l--) {
-          deltas[l] = getDeltaForLayer(answers[d], p, l, deltas[l+1]);
+          deltas[l] = getDeltaForLayer(l, deltas[l + 1]);
         }
-        layers[0].getWeights().plus(deltas[0].times(learning[d].transpose()).times(n));
-        for (int l = 1; l < countOfLayers; l++) {
-          layers[l].getWeights().plus(deltas[l].times(layers[l-1].getOutputs().transpose()).times(n));
+        changeWeights(n, learning[d], deltas);
+      }
+    }
+  }
+
+  private void changeWeights(double n, Vec learningVec, Vec[] deltas) {
+    List<Vec> outputs = new ArrayList<Vec>();
+    outputs.add(learningVec);
+    for (int i = 0; i < countOfLayers; i++) {
+      outputs.add(layers[i].getOutputs());
+    }
+    for (int l = 0; l < countOfLayers; l++) {
+      for (int i = 0; i < layers[l].getWeights().rows(); i++) {
+        for (int j = 0; j < layers[l].getWeights().columns(); j++) {
+          layers[l].getWeights().adjust(i, j, n * deltas[l].get(j) * outputs.get(l).get(i));
         }
       }
     }
   }
 
-  private Matrix getDeltaForLastLayer(Matrix answer, Function p) {
-    int neurons = layers[countOfLayers - 1].getCountOfNeurons();
-    Matrix delta = new Matrix(neurons, 1);
-    for (int j = 0; j < neurons; j++) {
-      double o = layers[countOfLayers - 1].getOutputs().get(j, 1);
-      double t = answer.get(j, 1);
-      delta.set(j, 1, p.call(o, t) * (o - o * o));
+  private Vec getDeltaForLastLayer(int answer) {
+    double result;
+    double o = layers[countOfLayers - 1].getOutputs().get(0);
+    if (answer == 1) {
+      result = 1 - o;
+    } else {
+      if (answer == -1) {
+        result = (o - o * o)/(o - 1);
+      } else {
+        throw new IllegalArgumentException("Answer must be only 1 or -1");
+      }
     }
-    return delta;
+    return new ArrayVec(result);
   }
 
-  private Matrix getDeltaForLayer(Matrix answer, Function p, int l, Matrix previousDelta) {
+  private Vec getDeltaForLayer(int l, Vec previousDelta) {
     int neurons = layers[l].getCountOfNeurons();
-    Matrix delta = new Matrix(neurons, 1);
+    VecBuilder vecBuilder = new VecBuilder(neurons);
     for (int j = 0; j < neurons; j++) {
-      double o = layers[l].getOutputs().get(j, 1);
-      double t = answer.get(j, 1);
-      Matrix weights = layers[l + 1].getWeights();
-      delta.set(j, 1, o * (1 - o) * weights.getMatrix(0, weights.getRowDimension(), j, j).transpose().times(previousDelta).det());
+      double o = layers[l].getOutputs().get(j);
+      Mx weights = layers[l + 1].getWeights();
+      vecBuilder.append(o * (1 - o) * multiply(weights.col(j), previousDelta));
     }
-    return delta;
+    return vecBuilder.build();
   }
 
-  private void initWeights(int inputDim, int outputDim) {
+  private void initWeights(int inputDim) {
     List<Integer> dims = new ArrayList<Integer>();
     dims.add(inputDim);
     dims.addAll(hiddenLayersDims.subList(0, hiddenLayersDims.size()));
-    dims.add(outputDim);
-    for (int l = 0; l <= hiddenLayersDims.size(); l++) {
-      Matrix w = new Matrix(dims.get(l + 1), dims.get(l));
+    dims.add(1);
+    for (int l = 0; l < countOfLayers; l++) {
+      Vec[] rows = new Vec[dims.get(l + 1)];
       for (int i = 0; i < dims.get(l + 1); i++) {
+        VecBuilder vecBuilder = new VecBuilder(dims.get(l));
         for (int j = 0; j < dims.get(l); j++) {
-          w.set(i, j, Math.random());
+          vecBuilder.append(Math.random());
         }
+        rows[i] = vecBuilder.build();
       }
-      layers[l] = new Lawyer(w, getActivateFunction());
+      Mx w = new RowsVecArrayMx(rows);
+      layers[l] = new Layer(w, getActivateFunction());
     }
   }
 
