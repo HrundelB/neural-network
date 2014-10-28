3cf552c62682e665cb189e5a0ebcb445cad2c084
Afonin Sergey <hrundelb@yandex.ru>
2014-10-29 01:38:09 +0300

    add - add write and read of matrix, test it

---
+++ src/main/java/ru/spbsu/apmath/neuralnetwork/StringTools.java
@@ -0,0 +1,67 @@
+package ru.spbsu.apmath.neuralnetwork;
+
+import com.spbsu.commons.math.vectors.Mx;
+import com.spbsu.commons.math.vectors.Vec;
+import com.spbsu.commons.math.vectors.impl.mx.RowsVecArrayMx;
+import com.spbsu.commons.math.vectors.impl.vectors.VecBuilder;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Афонин Сергей (hrundelb@yandex.ru)
+ * Date: 28.10.2014
+ * Time: 20:58
+ */
+public class StringTools {
+
+  public static String printMx(Mx mx) throws IOException {
+    StringWriter stringWriter = new StringWriter();
+    printMx(mx, stringWriter);
+    return stringWriter.toString();
+  }
+
+  public static void printMx(Mx mx, File file) throws IOException {
+    FileWriter fileWriter = new FileWriter(file);
+    printMx(mx, fileWriter);
+  }
+
+  public static void printMx(Mx mx, Writer writer) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < mx.rows(); i++) {
+      for (int j = 0; j < mx.columns(); j++) {
+        sb.append(mx.get(i,j)).append(' ');
+      }
+      sb.delete(sb.length() - 1, sb.length());
+      sb.append(System.lineSeparator());
+    }
+    sb.delete(sb.length() - 1, sb.length());
+    writer.write(sb.toString());
+    writer.close();
+  }
+
+  public static Mx readMx(File file) throws IOException {
+    return readMx(new FileReader(file));
+  }
+
+  public static Mx readMx(String s) throws IOException {
+    return readMx(new StringReader(s));
+  }
+
+  public static Mx readMx(Reader reader) throws IOException {
+    List<Vec> rows = new ArrayList<Vec>();
+    BufferedReader bufferedReader = new BufferedReader(reader);
+    String line;
+    while ((line = bufferedReader.readLine()) != null) {
+      VecBuilder vecBuilder = new VecBuilder();
+      String[] elements = line.split(" ");
+      for (String s: elements)
+        vecBuilder.append(Double.parseDouble(s));
+      rows.add(vecBuilder.build());
+    }
+    bufferedReader.close();
+    return new RowsVecArrayMx(rows.toArray(new Vec[]{}));
+  }
+}
---
+++ src/test/java/ru/spbsu/apmath/neuralnetwork/StringToolsTest.java
@@ -0,0 +1,49 @@
+package ru.spbsu.apmath.neuralnetwork;
+
+import com.spbsu.commons.math.vectors.Mx;
+import com.spbsu.commons.math.vectors.Vec;
+import com.spbsu.commons.math.vectors.impl.mx.RowsVecArrayMx;
+import com.spbsu.commons.math.vectors.impl.vectors.ArrayVec;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static ru.spbsu.apmath.neuralnetwork.MxEqualsMatcher.mxEqualsTo;
+import static ru.spbsu.apmath.neuralnetwork.StringTools.printMx;
+import static ru.spbsu.apmath.neuralnetwork.StringTools.readMx;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Афонин Сергей (hrundelb@yandex.ru)
+ * Date: 28.10.2014
+ * Time: 21:18
+ */
+public class StringToolsTest {
+
+  @Test
+  public void printMxTest() throws IOException {
+    Mx original = getMx();
+    String s = printMx(original);
+    Mx newMx = readMx(s);
+    assertThat(newMx, mxEqualsTo(original));
+  }
+
+  private Mx getMx() {
+    Vec vec1 = new ArrayVec(2, 5, 8);
+    Vec vec2 = new ArrayVec(1, 51, 81);
+    Vec vec3 = new ArrayVec(32, 52, 18);
+    Vec vec4 = new ArrayVec(200, 0.5858585855, 8.0000000000123);
+    return new RowsVecArrayMx(new Vec[]{vec1, vec2, vec3, vec4});
+  }
+
+  @Test
+  public void writeMxToFileTest() throws IOException {
+    Mx original = getMx();
+    File file = new File("src/test/data/perceptron/matrixtest1.txt");
+    printMx(original, file);
+    Mx newMx = readMx(file);
+    assertThat(newMx, mxEqualsTo(original));
+  }
+}
